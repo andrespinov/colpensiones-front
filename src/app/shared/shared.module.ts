5b7331fe6f3235a1001807feaad7e721129a6410
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2019-07-30 17:39:50 -0500

    Angular Material added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 
+// Modules
+import { MaterialModule } from './modules/material.module';
+
 @NgModule({
   imports: [
+    MaterialModule
   ],
   exports: [
+    MaterialModule
   ]
 })
 export class SharedModule {}
