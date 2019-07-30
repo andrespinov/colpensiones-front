01db8de55359be29fabf7b4723f9bbfea53e06a3
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2019-07-30 17:34:20 -0500

    Ng Bootstrap deleted

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { BootstrapComponentsModule } from './modules/bootstrap-components.module';
-
 @NgModule({
   imports: [
-    BootstrapComponentsModule
   ],
   exports: [
-    BootstrapComponentsModule
   ]
 })
 export class SharedModule {}
