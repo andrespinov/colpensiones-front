aea9036746624761cb430fd964217e34f721b505
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2019-07-29 22:48:51 -0500

    Basic form for independent workers added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +16,9 @@ import { SharedModule } from './shared/shared.module';
     IndependentWorkersComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    FormsModule,
     SharedModule
   ],
   providers: [],
