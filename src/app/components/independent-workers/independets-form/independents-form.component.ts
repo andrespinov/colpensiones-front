dd873fd5fd3f2567f7997ce8c37aa696c28d4303
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2019-07-31 14:04:29 -0500

    Form validation added

--- src/app/components/independent-workers/independets-form/independents-form.component.ts
+++ src/app/components/independent-workers/independets-form/independents-form.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { IndependentWorkerModel } from 'src/app/models/independent-worker.model';
 import { TypeModel } from 'src/app/models/type-model';
 import { TYPES } from 'src/app/shared/types';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'independents-form',
@@ -11,11 +11,11 @@ import { NgForm } from '@angular/forms';
 
 export class IndependentsFormComponent {
 
-  @ViewChild('workerForm', { static: true }) workerForm: NgForm;
-
   public documentTypes: TypeModel[];
   public genderTypes: TypeModel[];
   public worker: IndependentWorkerModel;
+
+  private workerForm: FormGroup;
 
   public constructor() {
     this.documentTypes = TYPES.DOCUMENT_TYPES;
@@ -23,13 +23,24 @@ export class IndependentsFormComponent {
     this.initWorkerData();
   }
 
+  get controlForm() { return this.workerForm; }
+
+  get controls() { return this.workerForm.controls; }
+
   public initWorkerData(): void {
     this.worker = new IndependentWorkerModel();
+    this.workerForm = new FormGroup({
+      birthdate: new FormControl(this.worker.birthdate, [Validators.required]),
+      email: new FormControl(this.worker.email, [Validators.email, Validators.required]),
+      documentNumber: new FormControl(this.worker.documentNumber, [Validators.required]),
+      documentType: new FormControl(this.worker.documentType, [Validators.required]),
+      fullName: new FormControl(this.worker.fullName, [Validators.required]),
+      gender: new FormControl(this.worker.gender, [Validators.required]),
+      job: new FormControl(this.worker.job, [Validators.required]),
+      monthlyIncome: new FormControl(this.worker.monthlyIncome, [Validators.required]),
+      telephone: new FormControl(this.worker.telephone, [Validators.required])
+    });
   }
 
-  public onSaveWorker(): void {
-    if (this.workerForm.valid) {
-      console.log(12345)
-    }
-  }
+  public onSaveWorker(): void {}
 }
