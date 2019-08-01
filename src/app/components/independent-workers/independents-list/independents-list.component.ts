55e27374392f05cc9a80bc0d0e5cd2365b412554
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2019-07-31 22:07:44 -0500

    Table and requests fixed

--- src/app/components/independent-workers/independents-list/independents-list.component.ts
+++ src/app/components/independent-workers/independents-list/independents-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IndependentWorkerModel } from 'src/app/models/independent-worker.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { IndependentWorkerService } from 'src/app/services/independent-worker.service';
 
 @Component({
     selector: 'app-independents-list',
@@ -8,12 +10,27 @@ import { IndependentWorkerModel } from 'src/app/models/independent-worker.model'
 
 export class IndependentsListComponent {
 
+  public columnsProps: string[];
   public tableColumns = [
-    { id: 'documentType', name: 'Tipo de Documento' },
-    { id: 'documentNumber', name: 'Documento' },
-    { id: 'fullName', name: 'Nombre' }
+    { prop: 'documentType', name: 'Tipo de Documento' },
+    { prop: 'documentNumber', name: 'Documento' },
+    { prop: 'fullName', name: 'Nombre' }
   ];
+  public tableDataSource: MatTableDataSource<IndependentWorkerModel>;
   public workers: IndependentWorkerModel[] = [];
 
-  public constructor() {}
+  public constructor(
+    private independentWorkerService: IndependentWorkerService
+  ) {
+    this.initData();
+    this.independentWorkerService.independentWorkersObservable.subscribe(() => {
+      this.initData();
+    });
+  }
+
+  public initData(): void {
+    this.workers = this.independentWorkerService.getIndependentWorkers();
+    this.tableDataSource = new MatTableDataSource(this.workers);
+    this.columnsProps = this.tableColumns.map(column => column.prop);
+  }
 }
--- src/app/services/independent-worker.service.ts
+++ src/app/services/independent-worker.service.ts
@@ -4,11 +4,15 @@ import { map } from 'rxjs/operators';
 import { IndependentWorkerModel } from '../models/independent-worker.model';
 import { environment } from 'src/environments/environment';
 import { Endpoints } from '../shared/endpoints';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable()
 export class IndependentWorkerService {
 
+  private independentWorkersSubject: Subject<boolean> = new Subject<boolean>();
   private independentWorkers: IndependentWorkerModel[] = [];
+
+  public independentWorkersObservable: Observable<boolean> = this.independentWorkersSubject.asObservable();
 
   public constructor(private http: HttpClient) {}
 
@@ -16,6 +20,7 @@ export class IndependentWorkerService {
     return this.http.post<IndependentWorkerModel>(environment.api + Endpoints.INDEPENDENT_WORKER, worker).pipe(
       map((createdWorker: IndependentWorkerModel) => {
         this.independentWorkers.push(createdWorker);
+        this.independentWorkersSubject.next(true);
       })
     );
   }
@@ -24,8 +29,13 @@ export class IndependentWorkerService {
     return this.http.get<IndependentWorkerModel[]>(environment.api + Endpoints.INDEPENDENT_WORKER).pipe(
       map((workers: IndependentWorkerModel[]) => {
         this.independentWorkers = [...workers];
+        this.independentWorkersSubject.next(true);
       })
     );
+  }
+
+  public getIndependentWorkers(): IndependentWorkerModel[] {
+    return this.independentWorkers;
   }
 
   public updateIndependentWorkerState(worker: IndependentWorkerModel, approved: boolean) {
@@ -37,6 +47,7 @@ export class IndependentWorkerService {
           w.documentNumber === updatedWorker.documentNumber
         );
         this.independentWorkers[workerIndex] = new IndependentWorkerModel(updatedWorker);
+        this.independentWorkersSubject.next(true);
       })
     );
   }
